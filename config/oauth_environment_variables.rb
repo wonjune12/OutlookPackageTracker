3f2406a52674594bd9552b7e6be09bfea11912bf
Wonjun <wonjune12@hotmail.com>
2019-02-27 17:00:41 -0500

    Removed oauth environment file from git tree

--- config/oauth_environment_variables.rb
+++
@@ -1,5 +0,0 @@
-ENV['AZURE_APP_ID'] = '8d1e9b06-b7ec-4284-9f92-6b35ea37ff17'
-ENV['AZURE_APP_SECRET'] = 'kkoUETP66:|clorJRB997;('
-ENV['AZURE_SCOPES'] = 'openid profile email offline_access user.read calendars.read mail.read'
-
-ENV['SHIPENGINE_API_KEY']="I7miILFQG3RmjYbksSS+NadPnxxu3OxtkjkxBAgELHI"
